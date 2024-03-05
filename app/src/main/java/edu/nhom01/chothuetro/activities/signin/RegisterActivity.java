27ab57fb53029d480a9125e5080a9870eff88336
Dannie203 <21dh113707@st.huflit.edu.vn>
2024-03-06 02:33:14 +0700

    update build config update misc mapped RegisterActivity

--- app/src/main/java/edu/nhom01/chothuetro/activities/signin/RegisterActivity.java
+++ app/src/main/java/edu/nhom01/chothuetro/activities/signin/RegisterActivity.java
@@ -1,14 +1,38 @@
 package edu.nhom01.chothuetro.activities.signin;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
 import androidx.appcompat.app.AppCompatActivity;
 import edu.nhom01.chothuetro.R;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private EditText txtRegisterCid;
+    private EditText txtRegisterName;
+    private ImageView btnDOB;
+    private EditText txtRegisterAddress;
+    private EditText txtRegisterUsrName;
+    private EditText txtRegisterPassword;
+    private EditText txtRegisterEmail;
+    private EditText txtRegisterPhone;
+    private RadioButton radioMale;
+    private RadioButton radioFemale;
+    private Button btnRegister;
 
     private void setComponents() {
-
+        this.txtRegisterCid = findViewById(R.id.txtRegisterCid);
+        this.txtRegisterName = findViewById(R.id.txtRegisterName);
+        this.btnDOB = findViewById(R.id.btnDob);
+        this.txtRegisterAddress = findViewById(R.id.txtRegisterAddress);
+        this.txtRegisterPhone = findViewById(R.id.txtRegisterPhone);
+        this.txtRegisterUsrName = findViewById(R.id.txtRegisterUsrName);
+        this.txtRegisterPassword = findViewById(R.id.txtRegisterPassword);
+        this.txtRegisterEmail = findViewById(R.id.txtRegisterEmail);
+        this.radioMale = findViewById(R.id.rbMale);
+        this.radioFemale = findViewById(R.id.rbFemale);
+        this.btnRegister = findViewById(R.id.btnRegisterAccount);
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,3 +42,5 @@ public class RegisterActivity extends AppCompatActivity {
         setComponents();
     }
 }
+
+
